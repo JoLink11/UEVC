4c49dcfc34d67e729d85766786607d4743320e5e
JoLink11 <johannes.link.11@googlemail.com>
2021-11-13 19:04:03 +0100

    Rev inplemented

--- src/main.cpp
+++ src/main.cpp
@@ -35,6 +35,7 @@ const char* password = "Aprilia_RS_125";
 // default setting for RAVE2
 static const int rpm_default = 8100; // rpm to Open
 static const int threshold_default = 100; // rpm_default - threshold = closing rpm
+static const int PulsesPerRevolution_default = 1; //default ppr #### 1 für Drehzahlmessung am Pickup oder Zündspule; 6 für Drehzahlmessung an der Lima (ROTAX)
 
 // SERVO DEF
 Servo ObjServo; // Make object of Servo motor from Servo library
@@ -61,19 +62,77 @@ int position5 = 0;
 int position6 = 0;
 int position7 = 0;
 int position8 = 0;
+int position9 = 0;
+int position10 = 0;
 
-String rpmString = "";
+String rpmopenString = "";
 String thresholdString  = "";
+int rpmclose;
 String positioncloseString = "";
 String positionopenString = "";
+String PulsesPerRevolutionString = "";
 
 // Mod LED DEF
-// Auxiliar variables to store the current output state
-String output19State = "off";
-// Assign output variables to GPIO pins
-const int output19 = 19;
+const int LED_PIN = 19;
 
-void setup() {
+// Mod Interrupt Pin
+const int interrupt_PIN = 2;
+
+/////////////////////////////////////////
+// FROM InterlinkKnight & el bodo es loco
+
+//Timeout & Genauigkeit//
+const unsigned long ZeroTimeout = 100000;  
+const byte numReadings = 5;  //Anzahl der Messwertblöcke zur Mittelwert bildung Je höher des to genauer aber auch trägerzwischen 3-10 ist in Ordnung
+//muss nicht verändert werden 
+ 
+/////////////
+// Variablen:
+/////////////
+
+volatile unsigned long LastTimeWeMeasured;  
+volatile unsigned long PeriodBetweenPulses = ZeroTimeout+1000;  
+volatile unsigned long PeriodAverage = ZeroTimeout+1000;  
+unsigned long FrequencyRaw;  
+unsigned long FrequencyReal; 
+unsigned long RPMEngine; 
+unsigned int PulseCounter = 1;  
+
+unsigned long PeriodSum; 
+unsigned long LastTimeCycleMeasure = LastTimeWeMeasured;  
+unsigned long CurrentMicros = micros(); 
+unsigned int AmountOfReadings = 1;
+unsigned int ZeroDebouncingExtra;  
+unsigned long readings[numReadings];  
+unsigned long readIndex;  
+unsigned long total;  
+unsigned long average; 
+/////////////////////////////////////////
+
+void Pulse_Event()
+{
+  PeriodBetweenPulses = micros() - LastTimeWeMeasured;  
+  LastTimeWeMeasured = micros();  
+
+ if(PulseCounter >= AmountOfReadings)  
+  {
+    PeriodAverage = PeriodSum / AmountOfReadings; 
+    PulseCounter = 1;  
+    PeriodSum = PeriodBetweenPulses; 
+    int RemapedAmountOfReadings = map(PeriodBetweenPulses, 40000, 5000, 1, 10);  
+    
+    RemapedAmountOfReadings = constrain(RemapedAmountOfReadings, 1, 10);
+    AmountOfReadings = RemapedAmountOfReadings;  
+  }
+  else
+  {
+    PulseCounter++; 
+    PeriodSum = PeriodSum + PeriodBetweenPulses; 
+  }
+} 
+
+void setup()
+{
 
   Serial.begin(115200);
 
@@ -81,21 +140,23 @@ void setup() {
   preferences.begin("RAVE2", false);
   // preferences.clear(); // if you want to clear all default values uncomment this line flash , comment and flash again!
   // Load Vars
-  rpmString = preferences.getUInt("rpm", rpm_default);
+  rpmopenString = preferences.getUInt("rpm", rpm_default);
   thresholdString = preferences.getUInt("threshold", threshold_default);
   positioncloseString = preferences.getUInt("positionclose", positionclose_default);
   positionopenString = preferences.getUInt("positionopen", positionopen_default);
+  PulsesPerRevolutionString = preferences.getUInt("PulsesPerRevolution", PulsesPerRevolution_default);
   preferences.end();
 
-  Serial.println(rpmString);
+  Serial.println(rpmopenString);
   Serial.println(thresholdString);
   Serial.println(positioncloseString);
   Serial.println(positionopenString);
+  Serial.println(PulsesPerRevolutionString);
   
   // Initialize the output variables as outputs
-  pinMode(output19, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
   // Set outputs to LOW
-  digitalWrite(output19, LOW);
+  digitalWrite(LED_PIN, LOW);
 
   ObjServo.attach(ServoGPIO); // it will attach the ObjServo to ServoGPIO pin which is 13 
 
@@ -113,9 +174,9 @@ void setup() {
   display.setContrast(255);
   display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
   display.setFont(ArialMT_Plain_10);
-  //display.drawString(display.getWidth() / 2, display.getHeight() / 2, "RAVE 2 CONTROL\nIP: 192.168.4.1\nOpen RPM: " + rpmString + "\nThreshold: " + thresholdString );
+  //display.drawString(display.getWidth() / 2, display.getHeight() / 2, "RAVE 2 CONTROL\nIP: 192.168.4.1\nOpen RPM: " + rpmopenString + "\nThreshold: " + thresholdString );
   //display.display();
-  display.drawString(display.getWidth() / 2, display.getHeight() / 2, "RAVE 2 CONTROL\n\nIP: 192.168.4.1\nOpen RPM: " + rpmString + "\nThreshold: " + thresholdString + "\nPosition_Close: "+ positioncloseString + "\nPosition_Open: "+ positionopenString );
+  display.drawString(display.getWidth() / 2, display.getHeight() / 2, "RAVE 2 CONTROL\n\nIP: 192.168.4.1\nOpen RPM: " + rpmopenString + "\nThreshold: " + thresholdString + "\nPosition_Close: "+ positioncloseString + "\nPosition_Open: "+ positionopenString );
   display.display();
   
   // Servo test
@@ -126,9 +187,16 @@ void setup() {
   ObjServo.write(positioncloseString.toInt());
   delay(500);
 
-}
+  /////////////////////////////////////////
+  // FROM InterlinkKnight & el bodo es loco
+  attachInterrupt(digitalPinToInterrupt(interrupt_PIN), Pulse_Event, RISING);  // changed GPIO_3 to GPIO_2
+  pinMode (interrupt_PIN, INPUT_PULLUP);
+  delay(500);
+  /////////////////////////////////////////
 
-void loop(){
+}
+void loop()
+{
 
   WiFiClient client = server.available();   // Listen for incoming clients
 
@@ -147,10 +215,11 @@ void loop(){
 		  {
 
       preferences.begin("RAVE2", false);
-      rpmString = preferences.getUInt("rpm", rpm_default);
+      rpmopenString = preferences.getUInt("rpm", rpm_default);
       thresholdString = preferences.getUInt("threshold", threshold_default);
       positioncloseString= preferences.getUInt("positionclose", positionclose_default);
       positionopenString= preferences.getUInt("positionopen", positionopen_default);
+
 
 			client.println("HTTP/1.1 200 OK");
 			client.println("Content-type:text/html");
@@ -167,13 +236,15 @@ void loop(){
 			client.println("<h1>RAVE2 Conrtol</h1>");
 			client.println("<form action=\"\" method=\"get\">");
 			client.println("<label for=\"rname\">RPM:</label>");
-			client.print("<input type=\"number\" id=\"rname\" name=\"rpm\" value="+ rpmString +"min=7000 max=9000 step=1><br><br>");
+			client.print("<input type=\"number\" id=\"rname\" name=\"rpm\" value="+ rpmopenString +"min=7000 max=9000 step=1><br><br>");
 			client.println("<label for=\"tname\">Threshold:</label>");
 			client.print("<input type=\"number\" id=\"tname\" name=\"threshold\" value="+ thresholdString +"min=100 max=500 step=1><br><br>");
       client.println("<label for=\"cname\">Servo Close Position:</label>");
 			client.print("<input type=\"number\" id=\"cname\" name=\"positionclose\" value="+ positioncloseString +"min=0 max=180 step=1><br><br>");
       client.println("<label for=\"oname\">Servo Open Position:</label>");
 			client.print("<input type=\"number\" id=\"oname\" name=\"positionopen\" value="+ positionopenString +"min=0 max=180 step=1><br><br>");
+      client.println("<label for=\"pname\">Pulse per Rev:</label>");
+			client.print("<input type=\"number\" id=\"pname\" name=\"\" value="+ PulsesPerRevolutionString +"min=1 max=999 step=1><br><br>");
 			client.println("<button type=\"submit\">Submit</button>");
 			client.println("</form>");
 			client.println("</body>");
@@ -189,28 +260,32 @@ void loop(){
 				position5 = header.indexOf('=', position3+1);
         position6 = header.indexOf('&', position5+1);
         position7 = header.indexOf('=', position5+1);
-        position8 = header.indexOf(' ', position7+1);
+        position8 = header.indexOf('&', position7+1);
+        position9 = header.indexOf('=', position7+1);
+        position10 = header.indexOf(' ', position9+1);
 
-				rpmString = header.substring(position1+1, position2);
+				rpmopenString = header.substring(position1+1, position2);
 				thresholdString = header.substring(position3+1, position4);
 				positioncloseString = header.substring(position5+1, position6);
 				positionopenString = header.substring(position7+1, position8);
+        PulsesPerRevolutionString = header.substring(position9+1, position10);
 
 				// Rotate the servo
-				ObjServo.write(positioncloseString.toInt());
+        // ObjServo.write(positioncloseString.toInt()); // test application
 
-        Serial.println(rpmString);
+        Serial.println(rpmopenString);
 				Serial.println(thresholdString);
         Serial.println(positioncloseString); 
 				Serial.println(positionopenString);
+        Serial.println(PulsesPerRevolutionString);
 
-        if (rpmString.toInt() >= 6999 && rpmString.toInt() <= 9001)
+        if (rpmopenString.toInt() >= 6999 && rpmopenString.toInt() <= 9001)
         {
-          preferences.putUInt("rpm", rpmString.toInt());
+          preferences.putUInt("rpm", rpmopenString.toInt());
         }
         else 
         {
-          rpmString = preferences.getUInt("rpm");
+          rpmopenString = preferences.getUInt("rpm");
         }
 
         if (thresholdString != "")
@@ -240,20 +315,33 @@ void loop(){
           positionopenString = preferences.getUInt("positionopen");
         }
 
+        if (PulsesPerRevolutionString != "")
+        {
+          preferences.putUInt("PulsesPerRevolution", PulsesPerRevolutionString.toInt()); 
+        }
+        else 
+        {
+          PulsesPerRevolutionString = preferences.getUInt("PulsesPerRevolution");
+        }
+
+
         preferences.end();
 
-        Serial.println("RPM: " + rpmString);
+        Serial.println("RPM: " + rpmopenString);
 				Serial.println("Threshold: " + thresholdString);
         Serial.println("Close: " + positioncloseString); 
 				Serial.println("Open: " + positionopenString);
+        Serial.println("PPR: " + PulsesPerRevolutionString);
                 
         display.init();
         display.setContrast(255);
         display.setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
-        display.drawString(display.getWidth() / 2, display.getHeight() / 2, "RAVE 2 CONTROL\n\nIP: 192.168.4.1\nOpen RPM: " + rpmString + "\nThreshold: " + thresholdString + "\nPosition_Close: "+ positioncloseString + "\nPosition_Open: "+ positionopenString );
+        display.drawString(display.getWidth() / 2, display.getHeight() / 2, "RAVE 2 CONTROL\n\nIP: 192.168.4.1\nOpen RPM: " + rpmopenString + "\nThreshold: " + thresholdString + "\nPosition_Close: "+ positioncloseString + "\nPosition_Open: "+ positionopenString);
         display.display();
 
-        digitalWrite(output19, HIGH); // LED ON => modified settings
+        rpmclose = rpmopenString.toInt() - thresholdString.toInt();
+
+        digitalWrite(LED_PIN, HIGH); // LED ON => modified settings
 			}
             
       // The HTTP response ends with another blank line
@@ -275,4 +363,51 @@ void loop(){
     Serial.println("Client disconnected.");
     Serial.println("");
   }
+
+  LastTimeCycleMeasure = LastTimeWeMeasured;
+    CurrentMicros = micros(); 
+    if(CurrentMicros < LastTimeCycleMeasure)
+    {
+        LastTimeCycleMeasure = CurrentMicros;
+    }
+
+    FrequencyRaw = 10000000000 / PeriodAverage;
+    if(PeriodBetweenPulses > ZeroTimeout - ZeroDebouncingExtra || CurrentMicros - LastTimeCycleMeasure > ZeroTimeout - ZeroDebouncingExtra)
+    {  
+        FrequencyRaw = 0;   
+        ZeroDebouncingExtra = 2000;
+    }
+    else
+    {
+        ZeroDebouncingExtra = 0;  
+    }
+    FrequencyReal = FrequencyRaw / 10000;  
+    RPMEngine = FrequencyRaw / PulsesPerRevolutionString.toInt() * 60;  
+    RPMEngine = RPMEngine / 10000; 
+    total = total - readings[readIndex];  
+    readings[readIndex] = RPMEngine;  
+    total = total + readings[readIndex];  
+    readIndex = readIndex + 1;  
+
+    if (readIndex >= numReadings) 
+    {
+        readIndex = 0;  
+    }
+    average = total / numReadings; 
+    // Setpoint = analogRead (PIN_ADJUST);  // Ausgeklammert für Feste Öffnungsdrehzahl
+    // set = map(Setpoint, 0, 1024, 6000, 10500 );  // Ausgeklammert für Feste Öffnungsdrehzahl
+    rpmclose = rpmopenString.toInt() - thresholdString.toInt() ;
+
+    if (RPMEngine <= rpmclose)
+    { // unter diesem Wert bleibt der Auslassschieber geschlossen
+      digitalWrite(LED_PIN,LOW); 
+      ObjServo.write(positioncloseString.toInt());
+        
+    }
+    if (RPMEngine >= rpmopenString.toInt())
+    { // über diesem Wert öffnet der Auslassschieber
+      digitalWrite(LED_PIN,HIGH);  
+      ObjServo.write(positionopenString.toInt());
+    }
+
 }
